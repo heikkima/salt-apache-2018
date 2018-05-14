671501d0cd6c45fa724ed3581cb727a877f13085
Heikki Ma <heikki.ma@webscale.fi>
2018-05-14 21:02:34 +0300

    cretes minion file before moving

--- master.sh
+++ master.sh
@@ -5,10 +5,11 @@ sudo apt-get update && sudo apt-get install -y salt-master salt-minion
 
 echo "Create new minion file"
 sudo mv /etc/salt/minion /etc/salt/minion.original
-cd /etc/salt/
+
 printf "master: " >> minion
 curl -s http://whatismyip.akamai.com/ >> minion
 printf "id: master"
+sudo mv minion /etc/salt/
 
 cd /srv/
 git clone -b final-module https://github.com/heikkima/salt-apache-2018.git salt
